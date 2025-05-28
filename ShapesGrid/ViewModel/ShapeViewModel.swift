0d1b74de0d2f3e1dd3a87a104bfe0717d04a5fe1
Parth Brahmbhatt <parthbrahmbhatt@Parths-MacBook-Pro.local>
2025-05-28 12:59:10 -0400

    Implement getButton function to get the dynamic shapes and conver it into SwiftModel object.

--- ShapesGrid/ViewModel/ShapeViewModel.swift
+++ ShapesGrid/ViewModel/ShapeViewModel.swift
@@ -9,4 +9,18 @@ import Foundation
 
 class ShapeViewModel: ObservableObject {
     @Published var buttons: [ShapeModel] = []
+    @Published var shapes: [String] = []
+    
+    func getButtons() async {
+        guard let url = URL(string: "http://staticcontent.cricut.com/static/test/shapes_001.json") else {
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            buttons = try JSONDecoder().decode([ShapeModel].self, from: data)
+        } catch {
+            print("Error:", error)
+        }
+    }
 }
